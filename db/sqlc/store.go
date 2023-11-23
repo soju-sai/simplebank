28bd87380f6815bd45cb959caa2021f9d564f10d
soju-sai <kiraarus@gmail.com>
2023-11-23 16:53:47 +0800

    refactor query order to avoid deadlock when updating

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -80,18 +80,11 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		result.FromAccount, err = q.AddAmountToAccount(ctx, AddAmountToAccountParams{
-			Amount: -arg.Amount,
-			ID:     arg.FromAccountID,
-		})
-		if err != nil {
-			return err
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = plusMoney(ctx, q, -arg.Amount, arg.FromAccountID, arg.Amount, arg.ToAccountID)
+		} else {
+			result.ToAccount, result.FromAccount, err = plusMoney(ctx, q, arg.Amount, arg.ToAccountID, -arg.Amount, arg.FromAccountID)
 		}
-
-		result.ToAccount, err = q.AddAmountToAccount(ctx, AddAmountToAccountParams{
-			Amount: arg.Amount,
-			ID:     arg.ToAccountID,
-		})
 		if err != nil {
 			return err
 		}
@@ -100,4 +93,28 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 	})
 
 	return result, err
+}
+
+func plusMoney(
+	ctx context.Context,
+	q *Queries,
+	amount1 int64,
+	accountID1 int64,
+	amount2 int64,
+	accountID2 int64,
+) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAmountToAccount(ctx, AddAmountToAccountParams{
+		Amount: amount1,
+		ID:     accountID1,
+	})
+	if err != nil {
+		return
+	}
+
+	account2, err = q.AddAmountToAccount(ctx, AddAmountToAccountParams{
+		Amount: amount2,
+		ID:     accountID2,
+	})
+
+	return
 }
