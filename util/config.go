36120f5ed976f1d6bcff43f75f602a0061ef5881
soju-sai <kiraarus@gmail.com>
2023-11-24 16:54:54 +0800

    check app.env is existed before use it

--- util/config.go
+++ util/config.go
@@ -1,6 +1,10 @@
 package util
 
-import "github.com/spf13/viper"
+import (
+	"os"
+
+	"github.com/spf13/viper"
+)
 
 type Config struct {
 	DBDriver      string `mapstructure:"DB_DRIVER"`
@@ -9,9 +13,11 @@ type Config struct {
 }
 
 func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)
-	viper.SetConfigName("app")
-	viper.SetConfigType("env")
+	if _, err := os.Stat(path + "/app.env"); err == nil {
+		viper.AddConfigPath(path)
+		viper.SetConfigName("app")
+		viper.SetConfigType("env")
+	}
 
 	viper.AutomaticEnv()
 
