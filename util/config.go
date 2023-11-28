812e4c28c9ddb0b9f971d2cd241dc32c2bf21e35
soju-sai <kiraarus@gmail.com>
2023-11-28 20:31:18 +0800

    fix typo in config file

--- util/config.go
+++ util/config.go
@@ -9,7 +9,7 @@ import (
 type Config struct {
 	DBDriver      string `mapstructure:"DB_DRIVER"`
 	DBSource      string `mapstructure:"DB_SOURCE"`
-	ServerAddress string `mapstruture:"SERVER_ADDRESS"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
